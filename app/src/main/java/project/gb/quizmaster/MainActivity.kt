4beca39b5dfa1f6c9bd9f666a2b8571fac290de6
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-02-22 14:06:28 +0300

    Initial commit

---
+++ app/src/main/java/project/gb/quizmaster/MainActivity.kt
@@ -0,0 +1,17 @@
+package project.gb.quizmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.gb.quizmaster.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
